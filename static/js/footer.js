0695e90ba8ba42cf670f74821e7f239b7b95bd01
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-03-25 11:48:13 +0530

    courierguru clone

---
+++ static/js/footer.js
@@ -0,0 +1,11 @@
+//var i= document.getElementById('footerDynamic');
+	
+	$('#footerDynamic').load('/static/reuse/footer.html');
+	//$('#footerDynamic').load('/static/reuse/footer.html');
+	
+
+//var j= document.getElementById('formDynamic');
+
+	$('#formDynamic').load('/static/reuse/form.html');
+	//$.getScript('/static/js/_cDetails.js');
+	
